a2a8388beb0602371440d3f0299494e360611b1c
Jzee21 <Jzee21b@gmail.com>
2020-04-21 10:29:28 +0900

    Server UI Template

---
+++ IOT_Project/src/javaNetwork/Jzee_MuiltiRoomServerUITest.java
@@ -0,0 +1,78 @@
+package javaNetwork;
+
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Jzee_MuiltiRoomServerUITest extends Application{
+	
+	private TextArea textarea;
+	private Button startBtn, stopBtn;
+	
+	private ExecutorService executor = Executors.newCachedThreadPool();
+	private ServerSocket server;
+	
+	public void displayText(String msg) {
+		Platform.runLater(() -> {
+			textarea.appendText(msg + "\n");
+		});
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		BorderPane root = new BorderPane();
+		root.setPrefSize(700, 500);
+		
+		textarea = new TextArea();
+		textarea.setEditable(false);
+		root.setCenter(textarea);
+		
+		// Bottom		
+		startBtn = new Button("Server Start");
+		startBtn.setPrefSize(200, 40);
+		startBtn.setOnAction((e) -> {
+			
+		});
+		
+		stopBtn = new Button("Server Stop");
+		stopBtn.setPrefSize(200, 40);
+		stopBtn.setOnAction((e) -> {
+			
+		});
+		
+		FlowPane bottom = new FlowPane();
+		bottom.setPrefSize(700, 40);
+		bottom.setPadding(new Insets(5,5,5,5));
+		bottom.setHgap(5);
+		bottom.getChildren().addAll(startBtn, stopBtn);
+		bottom.setAlignment(Pos.CENTER_RIGHT);
+		root.setBottom(bottom);		
+		
+		// Scene
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Multi Room Chat Client");
+		primaryStage.setOnCloseRequest((e) -> {
+			//
+		});
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
